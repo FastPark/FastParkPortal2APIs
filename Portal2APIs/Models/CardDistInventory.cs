3f6f71967a6ef537e63de1fe33002fb76981fd24
koldham <koldham@HOO6430U>
2016-07-15 09:33:47 -0400

    Added CardDistInventory model

---
+++ Portal2APIs/Models/CardDistInventory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal2APIs.Models
+{
+    public class CardDistInventory
+    {
+        public int CardFPNumber
+        {
+            get { return m_CardFPNumber; }
+            set { m_CardFPNumber = value; }
+        }
+        private int m_CardFPNumber;
+    }
+    public class CardHistoryId
+    {
+        public int CardHistoryId
+        {
+            get { return m_CardHistoryId; }
+            set { m_CardHistoryId = value; }
+        }
+        private int m_CardHistoryId;
+    }
+    public class CardValidationNumber
+    {
+        public int CardValidationNumber
+        {
+            get { return m_CardValidationNumber; }
+            set { m_CardValidationNumber = value; }
+        }
+        private int m_CardValidationNumber;
+    }
+    public class CardActive
+    {
+        public int CardActive
+        {
+            get { return m_CardActive; }
+            set { m_CardActive = value; }
+        }
+        private int m_CardActive;
+    }
+    
+}
