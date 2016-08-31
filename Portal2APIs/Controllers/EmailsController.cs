96d8683d40daf96e44a95d62a94e7b02efbae1c5
MGoode <MGoode@pca-star.com>
2016-08-31 16:57:33 -0400

    email api

---
+++ Portal2APIs/Controllers/EmailsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Portal2APIs.Models;
+using Portal2APIs.Common;
+using System.Net.Mail;
+
+namespace Portal2APIs.Controllers
+{
+    public class EmailsController : ApiController
+    {
+        [HttpPost]
+        [Route("api/CardDistHistorys/SendEmail")]
+        public void SendEmail(Email emailInfo)
+        {
+            MailMessage Message = new MailMessage(emailInfo.FromEmailAddress, emailInfo.ToEmailAddress);
+
+
+            Message.Subject = emailInfo.Subject;
+            Message.Body = emailInfo.Body;
+            Message.IsBodyHtml = true;
+
+
+            SmtpClient smtp = new SmtpClient();
+
+
+            smtp.Host = "192.168.0.53";
+            smtp.Send(Message);
+
+        }
+    }
---
+++ Portal2APIs/Models/Email.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal2APIs.Models
+{
+    public class Email
+    {
+        public string ToEmailAddress
+        {
+            get { return m_ToEmailAddress; }
+            set { m_ToEmailAddress = value; }
+        }
+        private string m_ToEmailAddress;
+
+        public string FromEmailAddress
+        {
+            get { return m_FromEmailAddress; }
+            set { m_FromEmailAddress = value; }
+        }
+        private string m_FromEmailAddress;
+
+        public string Body
+        {
+            get { return m_Body; }
+            set { m_Body = value; }
+        }
+        private string m_Body;
+
+        public string Subject
+        {
+            get { return m_Subject; }
+            set { m_Subject = value; }
+        }
+        private string m_Subject;
+    }
+}
