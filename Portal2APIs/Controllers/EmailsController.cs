0ff6ca2b7e30bf5a310864a418f5e8dc1f12dd2e
MGoode <MGoode@pca-star.com>
2016-09-01 08:41:35 -0400

    added httpresponse message to send email

--- Portal2APIs/Controllers/EmailsController.cs
+++ Portal2APIs/Controllers/EmailsController.cs
@@ -13,22 +13,33 @@ namespace Portal2APIs.Controllers
     public class EmailsController : ApiController
     {
         [HttpPost]
-        [Route("api/CardDistHistorys/SendEmail")]
-        public void SendEmail(Email emailInfo)
+        [Route("api/Emails/SendEmail/")]
+        public HttpResponseMessage SendEmail(Email emailInfo)
         {
-            MailMessage Message = new MailMessage(emailInfo.FromEmailAddress, emailInfo.ToEmailAddress);
+            try
+            {
+                MailMessage Message = new MailMessage(emailInfo.FromEmailAddress, emailInfo.ToEmailAddress);
 
 
-            Message.Subject = emailInfo.Subject;
-            Message.Body = emailInfo.Body;
-            Message.IsBodyHtml = true;
-
-
-            SmtpClient smtp = new SmtpClient();
-
-
-            smtp.Host = "192.168.0.53";
-            smtp.Send(Message);
+                Message.Subject = emailInfo.Subject;
+                Message.Body = emailInfo.Body;
+                Message.IsBodyHtml = true;
+                
+                SmtpClient smtp = new SmtpClient();
+                
+                smtp.Host = "192.168.0.53";
+                smtp.Send(Message);
+                
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, "Success");
+                return response;
+            }
+            catch
+            {
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.BadGateway, "Error");
+                return response;
+            }
+            
 
         }
     }
+}
