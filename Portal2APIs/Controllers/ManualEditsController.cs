c83de0e3018f53f4110e070ac70caa7377772d46
MGoode <MGoode@pca-star.com>
2016-09-02 14:55:26 -0400

    Created Manualedit api's

---
+++ Portal2APIs/Controllers/ManualEditsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Portal2APIs.Common;
+using System.Data.SqlClient;
+using Portal2APIs.Models;
+
+
+namespace Portal2APIs.Controllers
+{
+    public class ManualEditsController : ApiController
+    {
+        [HttpGet]
+        [Route("api/ManualEdits/ManualEditById/")]
+        public List<ManualEdit> ManualEditById(int id)
+        {
+            try
+            {
+                string strSQL = "";
+                clsADO thisADO = new clsADO();
+
+
+                strSQL = "Select * from dbo.ManualEdits where ManualEditID=" + id + "";
+                List<ManualEdit> list = new List<ManualEdit>();
+                thisADO.returnList(strSQL, false, ref list);
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent(ex.Message, System.Text.Encoding.UTF8, "text/plain"),
+                    StatusCode = HttpStatusCode.BadRequest
+                };
+                throw new HttpResponseException(response);
+            }
+
+        }
+
+        [HttpGet]
+        [Route("api/ManualEdits/ManualEditByMemberId/")]
+        public List<ManualEdit> ManualEditByMemberId(int id)
+        {
+            try
+            {
+                string strSQL = "";
+                clsADO thisADO = new clsADO();
+
+
+                strSQL = "Select * from dbo.ManualEdits where MemberId=" + id + "";
+                List<ManualEdit> list = new List<ManualEdit>();
+                thisADO.returnList(strSQL, false, ref list);
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent(ex.Message, System.Text.Encoding.UTF8, "text/plain"),
+                    StatusCode = HttpStatusCode.BadRequest
+                };
+                throw new HttpResponseException(response);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/ManualEdits/AddManualEdit/")]
+        public void AddManualEdit(ManualEdit man)
+        {
+            clsADO thisADO = new clsADO();
+            string strSQL = null;
+
+            strSQL = "INSERT INTO dbo.ManualEdits (MemberID,LocationID,ManualEditDate,SubmittedDate,PerformedBy, " +
+                     "SubmittedBy, ExplanationID,PointsChanged,CertificateNumber,ParkingTransactionNumber,CompanyID, " +
+                     "Notes,PerformedByUserID,SubmittedByUserID) " +
+                     "Values (" + man.MemberId + ", " + man.LocationId + ", '" + man.ManualEditDate + "', '" + man.SubMittedDate + "', '" +
+                     man.PerformedBy + "', '" + man.SubmittedBy + "', " + man.LocationId + ", " + man.PointsChanged + ", '" +
+                     man.CertificateNumber + "', '" + man.ParkingTransactionNumber + "', " + man.CompanyId + ", '" + man.Notes + "', '" +
+                     man.PerformedByUserId + "', '" + man.SubmittedByUserId + "'";
+
+        }
+
+        [HttpPost]
+        [Route("api/ManualEdits/UpdateManualEdit/")]
+        public void UpdateManualEdit(ManualEdit man)
+        {
+            clsADO thisADO = new clsADO();
+            string strSQL = null;
+
+            strSQL = "Update dbo.ManualEdits set MemberID = " + man.MemberId + ", locationID = " + man.LocationId +
+                    ", ManualEditDate = '" + man.ManualEditDate + "', SubmittedDate = '" + man.SubMittedDate + "', PerformedBy = " +
+                    man.PerformedBy + ", SubmittedBy = " + man.SubmittedBy + ", ExplanationID = " + man.ExplanationId +
+                    ", CertificateNumber = '" + man.CertificateNumber + "', ParkingTransactionNumber = '" + man.ParkingTransactionNumber +
+                    "', CompanyID = " + man.CompanyId + ", Notes = '" + man.Notes + "', PointsChanged = " + man.PointsChanged +
+                    " Where ManualEditID = " + man.ManualEditId;
+
+            thisADO.updateOrInsert(strSQL, true);
+        }
+
+        [HttpDelete]
+        [Route("api/ManualEdits/DeleteManualEditByID/")]
+        public void DeleteManualEditByID(int id)
+        {
+            clsADO thisADO = new clsADO();
+            string strSQL = null;
+
+            strSQL = "delete from dbo.ManualEdits where ManualEditID=" + id;
+
+            thisADO.updateOrInsert(strSQL, true);
+            
+        }
+    }
+}
+
---
+++ Portal2APIs/Models/ManualEdit.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal2APIs.Models
+{
+    public class ManualEdit
+    {
+        public int ManualEditId
+        {
+            get { return m_ManualEditId; }
+            set { m_ManualEditId = value; }
+        }
+        private int m_ManualEditId;
+
+        public int MemberId
+        {
+            get { return m_MemberId; }
+            set { m_MemberId = value; }
+        }
+        private int m_MemberId;
+
+        public int LocationId
+        {
+            get { return m_LocationId; }
+            set { m_LocationId = value; }
+        }
+        private int m_LocationId;
+
+        public DateTime ManualEditDate
+        {
+            get { return m_ManualEditDate; }
+            set { m_ManualEditDate = value; }
+        }
+        private DateTime m_ManualEditDate;
+
+        public DateTime SubMittedDate
+        {
+            get { return m_SubMittedDate; }
+            set { m_SubMittedDate = value; }
+        }
+        private DateTime m_SubMittedDate;
+
+        public int PerformedBy
+        {
+            get { return m_PerformedBy; }
+            set { m_PerformedBy = value; }
+        }
+        private int m_PerformedBy;
+
+        public int SubmittedBy
+        {
+            get { return m_SubmittedBy; }
+            set { m_SubmittedBy = value; }
+        }
+        private int m_SubmittedBy;
+
+        public int ExplanationId
+        {
+            get { return m_ExplanationId; }
+            set { m_ExplanationId = value; }
+        }
+        private int m_ExplanationId;
+
+        public int PointsChanged
+        {
+            get { return m_PointsChanged; }
+            set { m_PointsChanged = value; }
+        }
+        private int m_PointsChanged;
+
+        public string CertificateNumber
+        {
+            get { return m_CertificateNumber; }
+            set { m_CertificateNumber = value; }
+        }
+        private string m_CertificateNumber;
+
+        public string ParkingTransactionNumber
+        {
+            get { return m_ParkingTransactionNumber; }
+            set { m_ParkingTransactionNumber = value; }
+        }
+        private string m_ParkingTransactionNumber;
+
+        public int CompanyId
+        {
+            get { return m_CompanyId; }
+            set { m_CompanyId = value; }
+        }
+        private int m_CompanyId;
+
+        public string Notes
+        {
+            get { return m_Notes; }
+            set { m_Notes = value; }
+        }
+        private string m_Notes;
+
+        public Guid PerformedByUserId
+        {
+            get { return m_PerformedByUserId; }
+            set { m_PerformedByUserId = value; }
+        }
+        private Guid m_PerformedByUserId;
+
+        public Guid SubmittedByUserId
+        {
+            get { return m_SubmittedByUserId; }
+            set { m_SubmittedByUserId = value; }
+        }
+        private Guid m_SubmittedByUserId;
+
+    }
+}
