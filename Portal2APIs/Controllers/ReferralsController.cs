bb2ac78e9b270bc33afa968106394ed7b8f8fdb6
MGoode <MGoode@pca-star.com>
2018-07-30 08:51:28 -0400

    referrals api

---
+++ Portal2APIs/Controllers/ReferralsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Portal2APIs.Common;
+using Portal2APIs.Models;
+
+namespace Portal2APIs.Controllers
+{
+    public class ReferralsController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Referrals/GetReferrals/{id}")]
+        public List<Referral> GetReferrals(string id)
+        {
+            try
+            {
+                string strSQL = "";
+                clsADO thisADO = new clsADO();
+
+                strSQL = "Select mi.MemberId, r.ReferralId, rt.ReferralTypeName, r.SentToEmail, r.CreateDatetime As RefferedDate, mi.MemberSince as SignedDate, mi.ReferralPointsAwarded, DATEADD(DAY, -1, mi.ReferralCompleteDate) " +
+                        "from referral r " +
+                        "Inner Join ReferralType rt on r.ReferralTypeId = rt.ReferralTypeId " +
+                        "Left Outer Join MemberInformationMain mi on r.ReferralId = mi.ReferralId " +
+                        "where r.MemberId = " + id + " " +
+                        "order by r.CreateDatetime desc";
+
+                List<Referral> list = new List<Referral>();
+                thisADO.returnSingleValue(strSQL, true, ref list);
+
+                return list;
+            }
+            catch (Exception ex)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.NotFound)
+                {
+                    Content = new StringContent(ex.Message, System.Text.Encoding.UTF8, "text/plain"),
+                    StatusCode = HttpStatusCode.BadRequest
+                };
+                throw new HttpResponseException(response);
+            }
+        }
+    }
+}
---
+++ Portal2APIs/Models/Referral.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portal2APIs.Models
+{
+    public class Referral
+    {
+
+        #region Constructors
+        public Referral()
+        {
+        }
+        #endregion
+        #region Private Fields
+        private int _ReferralId;
+        private Int64 _MemberId;
+        private string _ReferralTypeName;
+        private string _SentToEmail;
+        private DateTime _RefferedDate;
+        private DateTime _ReferralCompleteDate;
+        private DateTime _SignedDate;
+        private int _ReferralPointsAwarded;
+        #endregion
+        #region Public Properties
+        public int ReferralId
+        {
+            get { return _ReferralId; }
+            set { _ReferralId = value; }
+        }
+        public Int64 MemberId
+        {
+            get { return _MemberId; }
+            set { _MemberId = value; }
+        }
+        public string ReferralTypeName
+        {
+            get { return _ReferralTypeName; }
+            set { _ReferralTypeName = value; }
+        }
+        public string SentToEmail
+        {
+            get { return _SentToEmail; }
+            set { _SentToEmail = value; }
+        }
+        public DateTime RefferedDate
+        {
+            get { return _RefferedDate; }
+            set { _RefferedDate = value; }
+        }
+        public DateTime ReferralCompleteDate
+        {
+            get { return _ReferralCompleteDate; }
+            set { _ReferralCompleteDate = value; }
+        }
+        public DateTime SignedDate
+        {
+            get { return _SignedDate; }
+            set { _SignedDate = value; }
+        }
+        public int ReferralPointsAwarded
+        {
+            get { return _ReferralPointsAwarded; }
+            set { _ReferralPointsAwarded = value; }
+        }
+        #endregion
+    }
+}
